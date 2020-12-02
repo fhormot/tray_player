cae63ae9860403019c01d308b747df9beeee9cdf
Filip Hormot <f.hormot@gmail.com>
2020-12-02 21:01:09 +0100

    Used Electrate to generate an electron-react template

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className={'hello'}>
+                <h2>Hello Electrate</h2>
+                <img src="./assets/logo.png" />
+                <h4>A basic Electron + React.js template</h4>
+                <h4>Have Fun!</h4>
+            </div>
+        );
+    }
+}
